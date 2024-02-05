48bc4f330db25317b8e2c9b4821b112102398dd8
“Federico <federicomanna01@yahoo.it>
2024-02-05 18:23:22 +0100

    create one func for all items and improvements

--- script.js
+++ script.js
@@ -24,42 +24,56 @@ function loadData(string) {
         .then((response) => response.json())
         .then((result) => {
             if (string === "eminem") {
-                createHtml(result)
+                createHtml(result, 0)
             } else if (string === "metallica") {
-                createHtml2(result)
+                createHtml(result, 1)
             } else if (string === "queen") {
-                createHtml3(result)
+                createHtml(result, 2)
             }
         })
         .catch((error) => console.log("error detected :", error))
 }
 
-function createHtml(result) {
+function createHtml(result, artistIndex) {
     //scrivo html
     let allArray = result.data;
     console.log(allArray)
     let array = result.data[0];
     console.log(array)
+
+    let subContainer;
+    let modalTitleIndex;
+
     // container.style.display = "block";
-    let picEm = array.artist.picture;
+    if (artistIndex === 0) {
+        subContainer = subContainerEm;
+        modalTitleIndex = 0;
+    } else if (artistIndex === 1) {
+        subContainer = subContainerMet;
+        modalTitleIndex = 1;
+    } else if (artistIndex = 2) {
+        subContainer = subContainerQueen;
+        modalTitleIndex = 2;
+    }
+    let pic = array.artist.picture;
     let myImg = document.createElement("img");
-    myImg.src = picEm;
-    subContainerEm.appendChild(myImg);
+    myImg.src = pic;
+    subContainer.appendChild(myImg);
     let album = array.album.title;
     let myAlbum = document.createElement("div");
     myAlbum.innerText = album;
     myAlbum.className = "m-5";
-    subContainerEm.appendChild(myAlbum);
+    subContainer.appendChild(myAlbum);
     let modalBtn = document.createElement("button");
     modalBtn.style.display = "block";
     modalBtn.style.margin = "auto";
     modalBtn.style.marginTop = "20px";
-    modalTitle[0].innerText = album;
-    modalTitle[0].style.color = "black";
+    modalTitle[modalTitleIndex].innerText = album;
+    modalTitle[modalTitleIndex].style.color = "black";
     modalBtn.innerText = "Brani";
     modalBtn.className = "btn btn-outline-light";
     modalBtn.setAttribute("data-toggle", "modal");
-    modalBtn.setAttribute("data-target", "#emModal");
+    modalBtn.setAttribute("data-target", `#${result.data[0].artist.name.toLowerCase()}Modal`);
     myAlbum.appendChild(modalBtn);
     for (const songs of allArray) {
         console.log(songs.title)
@@ -67,87 +81,13 @@ function createHtml(result) {
         let myli = document.createElement("li");
         myli.innerText = allSongs;
         myli.style.color = "black";
-        modalBody[0].appendChild(myli);
+        modalBody[modalTitleIndex].appendChild(myli);
     }
 }
 
-function createHtml2(result) {
-    //scrivo html
-    let allArray = result.data;
-    console.log(allArray)
-    let array = result.data[0];
-    console.log(array)
-    // container.style.display = "block";
-    let picEm = array.artist.picture;
-    let myImg = document.createElement("img");
-    myImg.src = picEm;
-    subContainerMet.appendChild(myImg);
-    let album = array.album.title;
-    let myAlbum = document.createElement("div");
-    myAlbum.innerText = album;
-    myAlbum.className = "m-5";
-    subContainerMet.appendChild(myAlbum);
-    let modalBtn = document.createElement("button");
-    modalBtn.style.display = "block";
-    modalBtn.style.margin = "auto";
-    modalBtn.style.marginTop = "20px";
-    modalTitle[1].innerText = album;
-    modalTitle[1].style.color = "black";
-    modalBtn.innerText = "Brani";
-    modalBtn.className = "btn btn-outline-light";
-    modalBtn.setAttribute("data-toggle", "modal");
-    modalBtn.setAttribute("data-target", "#metModal");
-    myAlbum.appendChild(modalBtn);
-    for (const songs of allArray) {
-        console.log(songs.title)
-        let allSongs = songs.title;
-        let myli = document.createElement("li");
-        myli.innerText = allSongs;
-        myli.style.color = "black";
-        modalBody[1].appendChild(myli);
-    }
-}
-
-
-function createHtml3(result) {
-    //scrivo html
-    let allArray = result.data;
-    console.log(allArray)
-    let array = result.data[0];
-    console.log(array)
-    // container.style.display = "block";
-    let picEm = array.artist.picture;
-    let myImg = document.createElement("img");
-    myImg.src = picEm;
-    subContainerQueen.appendChild(myImg);
-    let album = array.album.title;
-    let myAlbum = document.createElement("div");
-    myAlbum.innerText = album;
-    myAlbum.className = "m-5";
-    subContainerQueen.appendChild(myAlbum);
-    let modalBtn = document.createElement("button");
-    modalBtn.style.display = "block";
-    modalBtn.style.margin = "auto";
-    modalBtn.style.marginTop = "20px";
-    modalTitle[2].innerText = album;
-    modalTitle[2].style.color = "black";
-    modalBtn.innerText = "Brani";
-    modalBtn.className = "btn btn-outline-light";
-    modalBtn.setAttribute("data-toggle", "modal");
-    modalBtn.setAttribute("data-target", "#queenModal");
-    myAlbum.appendChild(modalBtn);
-    for (const songs of allArray) {
-        console.log(songs.title)
-        let allSongs = songs.title;
-        let myli = document.createElement("li");
-        myli.innerText = allSongs;
-        myli.style.color = "black";
-        modalBody[2].appendChild(myli);
-    }
-}
 
 btnSearch.addEventListener("click", () => {
-    loadData(searchBar.value)
+    loadData(searchBar.value.toLowerCase())
     subContainerEm.innerHTML = "";
     subContainerMet.innerHTML = "";
     subContainerQueen.innerHTML = "";
